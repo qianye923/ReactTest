da351f771d28126d632cc34866affa88aad0b7f6
lcd <1006196296@qq.com>
2018-03-02 11:36:26 +0800

    第二次提交

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>今天是2018年的3月2号</p>
+        <p>今天是2018年的3月2号,v_chfli添加了些东西</p>
+        
       </div>
     );
   }
